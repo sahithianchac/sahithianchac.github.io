6a5390f4fedfdab068e844d64d0769d62c557bd0
sahithianchac <sahithianchac@gmail.com>
2021-03-30 17:17:34 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6e8b8b50b9d99a590f0b.js"
+    "url": "webpack-runtime-631f875db7e601bd9728.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "85e98fea10342f074369437d22ca3e0c"
+    "revision": "44361135cab8923560abe810cba65ed7"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "371909f49ee9639598cec1da345da888"
+    "revision": "5dd8398744ef29d3ab4c1973d69efe74"
   },
   {
     "url": "polyfill-e4957c11759bdfc149d9.js"
